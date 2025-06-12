7842dc8d74bc61158e060c8ba9a3629685ab242a
margs163 <danial.aldanov@gmail.com>
2025-06-12 19:13:02 +0500

    preview changed

--- app/page.tsx
+++ app/page.tsx
@@ -13,8 +13,8 @@ export default function Home() {
     <main className="flex flex-col gap-12 sm:gap-16 py-24 sm:py-32 lg:py-36 max-w-[370px] sm:max-w-[600px] md:max-w-[680px] lg:max-w-[900px] xl:max-w-[1200px] mx-auto">
       <Hero />
       <Location />
-      <About />
       <Appointment />
+      <About />
       <Services />
       <CheckUp />
       <Specialists />
